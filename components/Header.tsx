630e785873ce787b1e39f9ade9f79083208fa85b
DivyanshWriteCode <divyanshdocode@gmail.com>
2025-02-23 03:56:32 +0530

    Hi

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,36 @@
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import React from 'react'
-import { Button } from './ui/button'
-import { FilePlus2 } from 'lucide-react'
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import React from "react";
+import Link from "next/link"; // Import Link from Next.js
+import { Button } from "./ui/button";
+import { FilePlus2 } from "lucide-react";
 
 function Header() {
-    return (
-        <div className='flex justify-between p-5 bg-white shadow-sm'>
-            <a href='/' className='text-2xl'><span className="text-indigo-500 font-bold">Inksight</span></a>
-            <SignedIn>
-                <div className='flex items-center space-x-2'>
+  return (
+    <div className="flex justify-between p-5 bg-white shadow-sm">
+      <Link href="/" className="text-2xl">
+        <span className="text-indigo-500 font-bold">Inksight</span>
+      </Link>
+      <SignedIn>
+        <div className="flex items-center space-x-2">
+          <Button asChild variant="link" className="hidden md:flex">
+            <Link href="/dashboard/upgrade">Pricing</Link>
+          </Button>
 
-                    <Button asChild variant="link" className='hidden md:flex'>
-                        <a href='/dashboard/upgrade'>Pricing</a>
-                    </Button>
+          <Button asChild variant="outline">
+            <Link href="/dashboard">My Document</Link>
+          </Button>
 
-                    <Button asChild variant="outline">
-                        <a href='/dashboard'>My Document</a>
-                    </Button>
+          <Button asChild variant="outline">
+            <Link href="/dashboard/upload">
+              <FilePlus2 className="text-indigo-600" />
+            </Link>
+          </Button>
 
-                    <Button asChild variant="outline">
-                        <a href='/dashboard/upload'>
-                            <FilePlus2 className='text-indigo-600' />
-                        </a>
-                    </Button>
-
-                    <UserButton />
-                </div>
-            </SignedIn>
+          <UserButton />
         </div>
-    )
+      </SignedIn>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
